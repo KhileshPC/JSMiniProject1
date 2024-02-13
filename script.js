a2533b71c05cb8aeeb0d9b8f6e8042fc134646cb
KhileshPC <khilu0707@gmail.com>
2024-02-13 18:56:43 +0530

    js code complete for pwd generator

--- script.js
+++ script.js
@@ -91,18 +91,115 @@ function calcStrength(){
        
     }
 
+    function shufflePassword(array){
+        //fisher yates method
+        for(let i= array.length-1; i>0; i--){
+            const j = Math.floor(Math.random() * (i+1));
+            const temp = array[i];
+            array[j] = temp;
+        }
+        let str = "";
+        array.forEach((el) => (str += el));
+        return str;
+    }
 
-inputSlider.addEventListener('input', (e) => {
+        function handleCheckBoxChange() {
+            checkCount = 0;
+            allCheckBox.forEach( (checkbox) => {
+                if(checkbox.checked)
+                checkCount++;
+            });
+            
+            //special conditon
+            if(passwordLength < checkCount){
+                passwordLength = checkCount;
+                handleSlider();
+            }
+        }
+
+        allCheckBox.forEach( (checkBox) => {
+            checkBox.addEventListener('change' , handleCheckBoxChange);
+        })
+
+inputSlider.addEventListener ('input', (e) => {
     passwordLength = e.target.value;
     handleSlider();
 })
 
-copyBtn.addEventListener('click', ()=> {
-    if(passwordDisplay.vlaue){
+copyBtn.addEventListener('click', () => {
+    if(passwordDisplay.value)
         copyContent();
-    }
 })
 
 generateBtn.addEventListener('click' , ()=>{
+    //none of th checkbox
+    if(checkCount == 0)
+     return;
+
+    if(passwordLength < checkCount){
+        passwordLength = checkCount;
+        handleSlider();
+    }
+
+
+    //let statrt journey to find new password
+    console.log("starting tje journey");
+
+    //remove old password
+    password = "";
+
+    //lets put the stuff mentinoned by checkbox
+    // if(uppercaseCheck.checked){
+    //     password += generateUpperrCase();
+    // }
+
+    // if(lowercaseCheck.checked){
+    //     password += generateLowerCase();
+    // }
+
+    // if(numbersCheck.checked){
+    //     password += generateRandomNumber();
+    // }
+
+    // if(symbolsCheck.checked){
+    //     password += generateSymbol();
+    // }
+
+    let funcArr = [];
+    if(uppercaseCheck.checked)
+        funcArr.push(generateUpperrCase);
+
+        if(lowercaseCheck.checked)
+        funcArr.push(generateLowerCase);
+
+        if(symbolsCheck.checked)
+        funcArr.push(generateSymbol);
+
+        if(numbersCheck.checked)
+        funcArr.push(generateRandomNumber);
+
+        // compulsory addition
+        for(let i=0;i<funcArr.length;i++){
+            password <= funcArr[i]();
+        }
+        console.log("compulsory addition done");
+
+        //remaining  addition
+        for(let i=0;i<passwordLength-funcArr.length;i++){
+            let randIndex = getRndInteger(0, funcArr.length);
+            console.log("randindex" + randIndex);
+            password += funcArr[randIndex]();
+        }
+            console.log("Remaning addition done");
+        //shuffle the password
+        password = shufflePassword(Array.from(password));
+        console.log("shuffling done");
+
+        //show in UI
+        passwordDisplay.value = password;
+        console.log("UI addition done");
+
+        //calulate strength
+        calcStrength();
     
 })
